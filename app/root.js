fd55a8409d7946831bc0f3f3cd256aa6118bc389
aliismael128 <aliismael162@gmail.com>
2024-08-07 21:25:35 +0200

    rien

---
+++ app/root.js
@@ -0,0 +1,13 @@
+// import Image from "next/image";
+// import styles from "./page.module.css";
+
+export default function root() {
+  return (
+    <main className={styles.main}>
+      <div>
+        hello word
+
+      </div>
+    </main>
+  );
+}
